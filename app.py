70ce75bc8fabb9054b401dec26429bcf01112ace
Bekezhan Issabek <bekezhani44@mail.ru>
2021-06-08 20:56:21 +0600

    #2 Script

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import sys
 import camelot
+from tqdm import tqdm
 
 class Parser:
     def __init__(self, url: str) -> None:
@@ -6,16 +8,30 @@ class Parser:
     
     def get_kaspi_id(self):
         try:
+            ids_arr = []
             tables = camelot.read_pdf(self.url, pages='all')
-            # print(f"Total pages: {tables.n}")
-            for rows in tables:
+            print(f"Total pages: {tables.n}")
+            print("Parsing started...")
+            for rows in tqdm(tables):
                 for ids in rows.df.loc[1:, 3]:
-                    print(ids)
+                    ids_arr.append(ids)
+            
+            for i, id_ in enumerate(ids_arr):    
+                s = ""
+                ids_arr[i] = s.join(id_.split())
+            
+            self.arr_to_txt(ids_arr)
+
         except:
             print("file not found")
 
+    def arr_to_txt(self, source):
+        with open('result.txt', 'w') as f:
+            for item in source:
+                f.write("%s\n" % item)
 
 if __name__ == '__main__':
-    parse = Parser('../test.pdf')
+    url = str(sys.argv[1:][0])
+    parse = Parser(url)
     parse.get_kaspi_id()
     
